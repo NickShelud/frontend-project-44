ced77e84681cc9cf0c2733386555f8f72e6ac068
NickShelud <N.She.1997@mail.ru>
2024-02-18 11:28:16 +0300

    step 5 create brain-even game

---
+++ src/games/brain-even.js
@@ -0,0 +1,17 @@
+import launchGame from "../engine.js";
+
+export default function run() {
+  const taskDescription =
+    'Answer "yes" if the number is even, otherwise answer "no".';
+  let questionsAndAnswers = [];
+  for (let i = 0; i < 3; i += 1) {
+    const randomNumber = Math.floor(Math.random() * 100) + 1;
+    questionsAndAnswers[randomNumber] = randomNumber % 2 === 0 ? "yes" : "no";
+  }
+
+  launchGame(taskDescription, questionsAndAnswers);
+}
+
+function isEven(digit) {
+  return digit % 2 === 0 ? true : false;
+}
